ea0ee7540082d383a09fa21c07d749f7640af22a
elliot-esyt <5286170@student.gloscol.ac.uk>
2026-06-08 16:25:47 +0100

    added movement
    added a movement script

--- Plasma/Assets/Scripts/playerController.cs
+++ Plasma/Assets/Scripts/playerController.cs
@@ -1,17 +1,22 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class playerController : MonoBehaviour
-
 {
+    [SerializeField] private float moveSpeed = 5f;
 
-    void Start()
+    private void Update()
     {
+        var kb = Keyboard.current;
+        if (kb == null) return;
 
+        Vector2 move = Vector2.zero;
+
+        if (kb.wKey.isPressed || kb.upArrowKey.isPressed) move.y += 1;
+        if (kb.sKey.isPressed || kb.downArrowKey.isPressed) move.y -= 1;
+        if (kb.dKey.isPressed || kb.rightArrowKey.isPressed) move.x += 1;
+        if (kb.aKey.isPressed || kb.leftArrowKey.isPressed) move.x -= 1;
+
+        transform.Translate(move.normalized * moveSpeed * Time.deltaTime);
     }
-
-    void Update()
-    {
-
-    }
-
 }
