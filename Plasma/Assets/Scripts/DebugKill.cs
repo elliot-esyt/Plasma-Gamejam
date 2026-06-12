d1d65786526f274a98e2848243b9610197444d45
elliot-esyt <5286170@student.gloscol.ac.uk>
2026-06-12 13:41:48 +0100

    changed keybinds
    changed the keybinds in order to allow the weapons to be tested whilst still allowing the debug function

--- Plasma/Assets/Scripts/DebugKill.cs
+++ Plasma/Assets/Scripts/DebugKill.cs
@@ -5,7 +5,9 @@ public class DebugKill : MonoBehaviour // DEBUG SCRIPT TO INSTA KILL RANDOM ENEM
 {
     private void Update()
     {
-        if (Mouse.current.leftButton.wasPressedThisFrame) // gets left mouse button that frame
+        var kb = Keyboard.current;
+
+        if (kb.qKey.wasPressedThisFrame) // gets the Q key and see if it was pressed that frame
         {
             Enemy[] enemies = FindObjectsByType<Enemy>(FindObjectsSortMode.None); // finds an enemy
             if (enemies.Length == 0) return; // if theres no enemies then dont do anything
